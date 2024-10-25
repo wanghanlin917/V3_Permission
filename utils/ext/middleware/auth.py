002705fef66cf0269ae758327d207f7dff2a16d4
wanghanlin <wanghanlin917@163.com>
2024-10-25 23:38:14 +0800

    2024.10.25 home

--- utils/ext/middleware/auth.py
+++ utils/ext/middleware/auth.py
@@ -1,6 +1,6 @@
-
 from django.utils.deprecation import MiddlewareMixin
 from django.shortcuts import HttpResponse
+
 
 class AuthMiddleware(MiddlewareMixin):
     # def process_request(self, request):
@@ -9,11 +9,12 @@ class AuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.method == "OPTIONS":
             return HttpResponse("")
-    def process_respon(self, request,response):
-        #任意网址
+
+    def process_respon(self, request, response):
+        # 任意网址
         response["Access-Control-Allow-Origin"] = "*"
-        #任意的请求方式
+        # 任意的请求方式
         response["Access-Control-Allow-Methods"] = "*"
-        #允许任意的请求头
+        # 允许任意的请求头  
         response["Access-Control-Allow-Headers"] = "*"
         return response
