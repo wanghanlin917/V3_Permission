5a8fb60d108118bdd7a6cd75b562c67909d12d76
jk1997 <wanghanlin917@gmail.com>
2024-11-23 08:54:52 +0800

    2024.11.23 work

--- apps/shipper/views/address.py
+++ apps/shipper/views/address.py
@@ -1,6 +1,25 @@
-from utils.ext.mixins import ListRetrieveModelMixin
+from rest_framework import serializers
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.pagination import PageNumberPagination
 
+from apps.repository import models
+# from utils.ext.mixins import ListRetrieveModelMixin
+from utils.ext.mixins import ListPageNumberModelMixin
+from utils.ext.filter import MineBaseFilter
 
-class AddressView(ListRetrieveModelMixin, GenericViewSet):
-    pass
+class AddressPageNumberPagination(PageNumberPagination):
+    page_size = 20
+
+class MineFilter(MineBaseFilter):
+    MINE_FILED = "company_id"
+
+class AddressModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Address
+        fields = "__all__"
+
+class AddressView(ListPageNumberModelMixin, GenericViewSet):
+    filter_backends = [MineFilter,]
+    pagination_class = AddressPageNumberPagination
+    serializer_class = AddressModelSerializer
+    queryset = models.Address.objects.all().order_by('-id')
